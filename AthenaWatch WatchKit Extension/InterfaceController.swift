48f8f85017f09b8aaebc67549d1e968e05356cdc
Connor0p76 <kidsc2744@gmail.com>
2020-05-09 17:01:49 -0500

    added the actual thing lmao

---
+++ AthenaWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  AthenaWatch WatchKit Extension
+//
+//  Created by Connor Long on 4/10/20.
+//  Copyright © 2020 ConnorFX. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
